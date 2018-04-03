63e2ba50af8958f98d0710f08d7a936e2d703fe7
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-02 18:17:26 -0700

    Updating the navigation flow to start the app from Tutorial and welcome page. Ask for login and then go to Trip List Page.

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,14 +1,14 @@
 // The page the user lands on after opening the app and without a session
 // export const FirstRunPage = 'TutorialPage';
 // export const FirstRunPage = 'CardsPage';
-export const FirstRunPage = 'TabsPage';
+export const FirstRunPage = 'TutorialPage';
 // export const FirstRunPage = 'TripsMasterPage';
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
-export const MainPage = 'TabsPage';
+export const MainPage = 'WelcomePage';
 
 // The initial root pages for our tabs (remove if not using tabs)
-export const Tab1Root = 'TripsMasterPage';
-export const Tab2Root = 'SettingsPage';
+export const Tab1Root = 'TripListPage';
+export const Tab2Root = 'TripSearchPage';
 export const Tab3Root = 'TripSearchPage';
