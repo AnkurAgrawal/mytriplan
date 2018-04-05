19f8fa09e76b29d6c5c5986c0820b1ee14ed40cd
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-04 20:21:17 -0700

    Adding page variables.

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,14 +1,14 @@
 // The page the user lands on after opening the app and without a session
-// export const FirstRunPage = 'TutorialPage';
-// export const FirstRunPage = 'CardsPage';
+
 export const FirstRunPage = 'TutorialPage';
-// export const FirstRunPage = 'TripsMasterPage';
 
 // The main page the user will see as they use the app over a long period of time.
-// Change this if not using tabs
-export const MainPage = 'WelcomePage';
+export const MainPage = 'TabsPage';
+export const SignInPage = 'SignInPage';
+export const WelcomePage = 'WelcomePage';
+export const TutorialPage = 'TutorialPage';
 
 // The initial root pages for our tabs (remove if not using tabs)
 export const Tab1Root = 'TripListPage';
 export const Tab2Root = 'TripSearchPage';
-export const Tab3Root = 'TripSearchPage';
+export const Tab3Root = 'ProfilePage';
