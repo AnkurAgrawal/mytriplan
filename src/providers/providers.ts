34fc48328c9473c715e26d24e5d85fbba4ccf155
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-04 20:27:44 -0700

    Adding new providers in the list.

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,5 +1,6 @@
 import { Api } from './api/api';
 import { TripsProvider } from './trips/trips';
+import { AuthServiceProvider } from './auth-service/auth-service';
 // import { TripsProvider } from '../mocks/providers/trips';
 import { Settings } from './settings/settings';
 import { User } from './user/user';
@@ -7,6 +8,7 @@ import { User } from './user/user';
 import { PlanFormGeneratorProvider } from './plan-form-generator/plan-form-generator';
 import { AirportsProvider } from './airports/airports';
 import { FirestoreProvider } from './firestore/firestore';
+import { PhotoProvider } from './photo/photo';
 
 export {
     Api,
@@ -15,5 +17,7 @@ export {
     User,
     PlanFormGeneratorProvider,
     AirportsProvider,
-    FirestoreProvider
+    FirestoreProvider,
+    AuthServiceProvider,
+    PhotoProvider
 };
