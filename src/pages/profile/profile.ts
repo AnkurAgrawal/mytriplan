6556c3828341cc3387b4bb7b9440cbb925838e5d
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-06 08:38:27 -0700

    Initial style for profile page.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from 'firebase/app';
 
-import { AuthServiceProvider } from '../../providers/providers';
+import { AuthServiceProvider, PhotoProvider } from '../../providers/providers';
 
 /**
  * Generated class for the ProfilePage page.
@@ -17,15 +17,29 @@ import { AuthServiceProvider } from '../../providers/providers';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
+  @ViewChild('fileInput') fileInput;
+
   user: User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthServiceProvider, private photoProvider: PhotoProvider) {
     this.user = this.auth.getCurrentUser();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-    this.user.displayName;
+  ionViewDidLoad() { }
+
+  getPicture() {
+    this.photoProvider.getPicture().then(data => data? this.updateUserPhoto(data): this.fileInput.nativeElement.click());
   }
 
+  processWebPicture(event) {
+    this.photoProvider.processWebPicture(event.target.files[0]).then(data => this.updateUserPhoto(data));
+  }
+
+  updateUserPhoto(imageData: string) {
+    this.auth.updateUser({photoURL: imageData});
+  }
+
+  updateUserDisplayName(name: string) {
+    this.auth.updateUser({displayName: name}).then(data => console.log('User display name updated successfully.'));
+  }
 }
