b883bab9bedf6eb30081692ad0c80931c5c86bef
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-29 18:48:19 -0700

    Updating the maps page.

--- src/pages/map/map.module.ts
+++ src/pages/map/map.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
+
 import { MapPage } from './map';
 
 @NgModule({
@@ -8,6 +10,10 @@ import { MapPage } from './map';
   ],
   imports: [
     IonicPageModule.forChild(MapPage),
+    TranslateModule.forChild(),
   ],
+  exports: [
+    MapPage
+  ]
 })
 export class MapPageModule {}
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, LatLng, GoogleMapsEvent } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, LatLng, GoogleMapsEvent, MarkerOptions } from '@ionic-native/google-maps';
+import { NativeGeocoder, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
 
 /**
  * Generated class for the MapPage page.
@@ -18,46 +19,59 @@ export class MapPage {
 
   @ViewChild('map') mapElement: ElementRef;
   private map: GoogleMap;
-  private location:LatLng;
-  private locations: Array<LatLng> = [];
+  private markerOptions: Array<MarkerOptions> = [];
+  private address: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private platform: Platform) {
-    this.location = new LatLng(42.346903, -71.135101);
+  constructor(private navCtrl: NavController,
+    private navParams: NavParams,
+    private platform: Platform,
+    private nativeGeocoder: NativeGeocoder) {
+    this.address = this.navParams.get('address') as string;
 
-    this.locations.push(new LatLng(42.36, -71.1));
-    this.locations.push(new LatLng(42.359, -71.064));
-    this.locations.push(new LatLng(42.3206, -71.1829));
-    this.locations.push(new LatLng(42.3458, -71.1382));
+    this.markerOptions.push({position: new LatLng(42.346903, -71.135101)});
+    this.markerOptions.push({position: new LatLng(42.36, -71.1)});
+    this.markerOptions.push({position: new LatLng(42.359, -71.064)});
+    this.markerOptions.push({position: new LatLng(42.3206, -71.1829)});
+    this.markerOptions.push({position: new LatLng(42.3458, -71.1382)});
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
     this.platform.ready().then(() => {
-      let element = this.mapElement.nativeElement;
-      this.map = GoogleMaps.create(element);
+      this.nativeGeocoder.forwardGeocode(this.address)
+      .then((coordinates: NativeGeocoderForwardResult) => {
+        console.log('The coordinates are latitude=' + coordinates.latitude + ' and longitude=' + coordinates.longitude)
 
-      this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-        let options = {
-          target: this.location,
-          zoom: 1
-        };
+        let element = this.mapElement.nativeElement;
+        this.map = GoogleMaps.create(element);
 
-        this.map.moveCamera(options);
-        setTimeout(() => (this.addCluster()), 2000);
-      });
+        this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+          // this.addMarker({position: new LatLng(coordinates.latitude, coordinates.longitude)});
+          this.addMarker(this.markerOptions[0]);
+          this.map.moveCamera({
+            target: this.markerOptions[0].position,
+            zoom: 10
+          });
+          setTimeout(() => this.addCluster(this.markerOptions), 2000);
+        });
+      })
+      .catch((error: any) => console.log(error));
     });
   }
 
-  addMarker() {
-    this.map.addMarker({
-      title: 'My Marker',
+  isMarker(marker: LatLng | MarkerOptions): marker is MarkerOptions {
+    return (<MarkerOptions>marker).position !== undefined;
+  }
+
+  addMarker(marker: LatLng | MarkerOptions) {
+    let defaultMarkerOption: MarkerOptions = {
+      title: '',
       icon: 'blue',
       animation: 'DROP',
-      position: {
-        lat: this.location.lat,
-        lng: this.location.lng
-      }
-    })
+      position: new LatLng(0, 0)
+    };
+    this.isMarker(marker)? Object.assign(defaultMarkerOption, marker): defaultMarkerOption.position = marker;
+    this.map.addMarker(defaultMarkerOption)
     .then(marker => {
       marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
         alert('Marker Clicked');
@@ -65,11 +79,20 @@ export class MapPage {
     });
   }
 
-  addCluster() {
+  addCluster(markerOptions: Array<MarkerOptions>) {
     this.map.addMarkerCluster({
-      markers: this.locations,
+      markers: markerOptions,
       icons: [
-        {min: 2, max: 100, url: './assets/icon/', anchor: {x: 16, y: 16}},
+        {
+          min: 2,
+          max: 100,
+          url: './assets/icon/favicon.ico',
+          label: {},
+          anchor: {
+            x: 16,
+            y: 16
+          }
+        },
       ]
     })
     .then((markerCluster) => {
