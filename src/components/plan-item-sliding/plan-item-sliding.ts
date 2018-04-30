309008db5f9f6c9817c524baa89ad75d530bcf6c
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-29 19:05:05 -0700

    Updating template to improve usability, readablity and show more details. Add launch navigator support to open local maps application. Add sliding menu option to show route on maps application. Add icon color support.

--- src/components/plan-item-sliding/plan-item-sliding.ts
+++ src/components/plan-item-sliding/plan-item-sliding.ts
@@ -1,6 +1,9 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
 import { Plan } from '../../models/plan';
+import { Travel } from '../../models/travel';
 
 /**
  * Generated class for the PlanItemSlidingComponent component.
@@ -18,6 +21,43 @@ export class PlanItemSlidingComponent {
   @Output() open = new EventEmitter();
   @Output() delete = new EventEmitter();
 
-  constructor() { }
+  inactive: boolean = false;
 
+  constructor(private launchNavigator: LaunchNavigator, private platform: Platform) { }
+
+  ionViewDidLoad() { }
+
+  openAddressInMap(ev: Event) {
+    ev.stopPropagation();
+    console.log("Opening location in maps");
+
+    if (this.platform.is('core')) {
+      window.open('https://www.google.com/maps/search/?api=1&query=' + this.plan.address, '_system');
+    } else {
+      let options: LaunchNavigatorOptions = {};
+      this.launchNavigator.navigate(this.plan.address, options)
+      .then(
+        success => console.log('Launched navigator'),
+        error => console.log('Error launching navigator', error)
+      );
+    }
+  }
+
+  openRouteInMap(ev: Event) {
+    ev.stopPropagation();
+    console.log("Opening route in maps");
+
+    if (this.platform.is('core')) {
+      window.open('https://www.google.com/maps/dir/?api=1&origin=' + this.plan.address + '&destination=' + (<Travel> this.plan).destinationAddress, '_system');
+    } else {
+      let options: LaunchNavigatorOptions = {
+        start: this.plan.address
+      };
+      this.launchNavigator.navigate((<Travel> this.plan).destinationAddress, options)
+      .then(
+        success => console.log('Launched navigator'),
+        error => console.log('Error launching navigator', error)
+      );
+    }
+  }
 }
