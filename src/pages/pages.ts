2914c90b1536dc3d413d6c26ec27e9d987a10083
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-04-29 18:48:53 -0700

    Adding a page name variable for Splash page.

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,5 +1,6 @@
 // The page the user lands on after opening the app and without a session
 
+export const SplashPage = 'WelcomePage';
 export const FirstRunPage = 'TutorialPage';
 
 // The main page the user will see as they use the app over a long period of time.
