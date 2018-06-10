fd17f5ee24d1a5749de5876595254fb2ed4f20ac
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-06-10 16:36:32 -0700

    Updating query structure and adding new query fields.

--- src/providers/trips/trips.ts
+++ src/providers/trips/trips.ts
@@ -4,6 +4,7 @@ import { Query, CollectionReference } from '@firebase/firestore-types';
 import { Collection, Document } from 'angular-firestype';
 
 import { Trip, PartialTrip } from '../../models/trip';
+import { User } from '../../models/user';
 import { Api } from '../api/api';
 import { FirestoreProvider } from '../firestore/firestore';
 
@@ -47,49 +48,67 @@ export class TripsProvider {
     return this.firestore.getEditableDocument<Trip>(tripId, this.getTrips() || this.collectionName);
   }
 
-  query(params?: { [key: string]: { type: string, value: string | number } }): Observable<Trip[]> {
+  query(params?: {field: string, type: string, value: string | number }[]): Observable<Trip[]> {
     switch (this.currentDb) {
       default:
       case Database.Type.Firestore:
         return this.firestore.queryObservable<Trip>(this.collectionName, ref => {
           let query: CollectionReference | Query = ref;
-          Object.keys(params).forEach((key) => {
-            switch (params[key].type) {
+          params.forEach((param) => {
+            console.log(param);
+            switch (param.type) {
               case "substr":
-                query = query.orderBy(key);
-                // query.where(key, '>=', params[key].value).where(key, '<=', params[key].value + 'z');
+                query = query.orderBy(param.field);
                 break;
 
               case ">=":
-                query = query.orderBy(key);
+                query = query.orderBy(param.field);
                 break;
 
               case "<":
-                query = query.orderBy(key);
+                query = query.orderBy(param.field, 'desc');
+                break;
+
+              case "startAt":
+                query = query.orderBy(param.field);
+                break;
+
+              case "endBefore":
+                query = query.orderBy(param.field);
                 break;
 
               default:
-
                 break;
             }
           });
-          Object.keys(params).forEach((key) => {
-            switch (params[key].type) {
+          params.forEach((param) => {
+            switch (param.type) {
               case "substr":
-                query = query.startAt(params[key].value).endAt(params[key].value + 'z');
+                query = query.startAt(param.value).endAt(param.value + 'z');
                 // query.where(key, '>=', params[key].value).where(key, '<=', params[key].value + 'z');
                 break;
 
               case ">=":
-                query = query.where(key, '>=', params[key].value);
+                query = query.where(param.field, '>=', param.value);
                 break;
 
               case "<":
-                query = query.where(key, '<', params[key].value);
+                query = query.where(param.field, '<', param.value);
+                break;
+
+              case "==":
+                query = query.where(param.field, '==', param.value);
+                break;
+
+              case "startAt":
+                query = query.startAt(param.value);
+                break;
+
+              case "endBefore":
+                query = query.endBefore(param.value);
                 break;
 
               default:
-
                 break;
             }
           });
