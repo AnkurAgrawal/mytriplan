11534a5cf382d159882669e162289a35316e59ff
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-06-10 16:31:33 -0700

    Renaming the user provider.

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -3,7 +3,7 @@ import { TripsProvider } from './trips/trips';
 import { AuthServiceProvider } from './auth-service/auth-service';
 // import { TripsProvider } from '../mocks/providers/trips';
 import { Settings } from './settings/settings';
-import { User } from './user/user';
+import { UsersProvider } from './users/users';
 
 import { PlanFormGeneratorProvider } from './plan-form-generator/plan-form-generator';
 import { AirportsProvider } from './airports/airports';
@@ -15,7 +15,7 @@ export {
     Api,
     TripsProvider,
     Settings,
-    User,
+    UsersProvider,
     PlanFormGeneratorProvider,
     AirportsProvider,
     FirestoreProvider,
