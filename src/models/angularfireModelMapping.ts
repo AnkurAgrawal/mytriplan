039da6c43eef89d1de14397037b44d4f57804249
Ankur Agrawal <ankur1.agrawal@intel.com>
2018-06-10 16:34:16 -0700

    Updating model mapping as per modified database schema.

--- src/models/angularfireModelMapping.ts
+++ src/models/angularfireModelMapping.ts
@@ -1,5 +1,6 @@
 import { ModelType } from 'angular-firestype';
 import { Trip } from './trip';
+import { User } from './user';
 import { Itinerary } from './itinerary';
 
 export const modelMapping: {[key: string]: ModelType<any>} = {    // {[key: string]: ModelType<any>} for TypeScript type check
@@ -10,6 +11,20 @@ export const modelMapping: {[key: string]: ModelType<any>} = {    // {[key: stri
       itinerary: {
         type: Itinerary,
         arguments: ['plans']
+      }
+    },
+    options: {
+      timestampOnCreate: 'createdAt',
+      timestampOnUpdate: 'updatedAt'
+    }
+  },
+  users: {
+    type: User,
+    arguments: ['uid', 'email', 'displayName', 'photoURL', 'trips'],
+    subcollections: {
+      trips : {
+        type: Trip,
+        arguments: ['name', 'destination', 'dateFrom', 'dateTo', 'picture', 'description'],
       }
     },
     options: {
